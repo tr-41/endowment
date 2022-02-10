72a26fe8d3f226daa0dc8f1fe1c53ff537184f8c
tanrui <tanrui@qianxin.com>
2022-02-10 16:54:49 +0800

    1-init

---
+++ src/test/java/com/example/endowment/EndowmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endowment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndowmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
